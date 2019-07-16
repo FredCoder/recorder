e71fb7bfdeab5f322f4f27e1d09a4a0f374ba7af
chen-rongliang <chen-rongliang@qq.com>
2019-07-16 17:53:09 +0800

    Update recorder.js

--- recorder.js
+++ recorder.js
@@ -12,125 +12,134 @@ const utils = {
             bufferSize: 4096,       // 每个块的大小是4k
             inputChannelCount: 2,   // 输入为双声道
             outputChannelCount: 2   // 输出为双声道
-        }
+        };
 
-        return audioContext.createScriptProcessor(option.bufferSize, option.inputChannelCount, option.outputChannelCount)
+        return audioContext.createScriptProcessor(option.bufferSize, option.inputChannelCount, option.outputChannelCount);
     },
 
     mergeArray(list) {
 
-        let length = list.length * list[0].length
+        let length = list.length * list[0].length;
 
         let merge = new Float32Array(length),
-            offset = 0
+            offset = 0;
 
         for (let i = 0; i < list.length; i++) {
-            merge.set(list[i], offset)
-            offset += list[i].length
-        }
+            merge.set(list[i], offset);
+            offset += list[i].length;
+        };
 
-        return merge
+        return merge;
     },
 
     // 交叉合并左右声道数据
     mergeChannel(left, right) {
-        let length = left.length + right.length
-        let merge = new Float32Array(length)
+
+        let length = left.length + right.length;
+
+        let merge = new Float32Array(length);
+
         for (let i = 0; i < left.length; i++) {
-            let k = i * 2
-            merge[k] = left[i]
-            merge[k + 1] = right[i]
-        }
-        return merge
+            let k = i * 2;
+            merge[k] = left[i];
+            merge[k + 1] = right[i];
+        };
+
+        return merge;
     },
 
     writeUTFBytes(view, offset, string) {
-        let lng = string.length;
-        for (let i = 0; i < lng; i++) {
+
+        let length = string.length;
+
+        for (let i = 0; i < length; i++) {
             view.setUint8(offset + i, string.charCodeAt(i));
-        }
+        };
     }
-}
+};
 
 // wav音频文件构造函数
 class Wav {
     constructor(data) {
-        const WAV_HEAD_SIZE = 44
 
-        let buffer = new ArrayBuffer(data.length * 2 + WAV_HEAD_SIZE)
+        const WAV_HEAD_SIZE = 44;
+
+        let buffer = new ArrayBuffer(data.length * 2 + WAV_HEAD_SIZE);
 
         // 用DataView来操控buffer
-        this.view = new DataView(buffer)
+        this.view = new DataView(buffer);
 
         // 写入wav头部信息
-        this.writeHead(data.length * 2)
+        this.writeHead(data.length * 2);
 
         // 写入PCM数据
-        this.writeFile(data)
+        this.writeFile(data);
 
-        return buffer
+        return buffer;
     }
     writeHead(length) {
         // 写入wav头部信息
+
         // RIFF chunk descriptor/identifier
-        utils.writeUTFBytes(this.view, 0, 'RIFF')
+        utils.writeUTFBytes(this.view, 0, 'RIFF');
 
         // RIFF chunk length
-        this.view.setUint32(4, 44 + length, true)
+        this.view.setUint32(4, 44 + length, true);
 
         // RIFF type
-        utils.writeUTFBytes(this.view, 8, 'WAVE')
+        utils.writeUTFBytes(this.view, 8, 'WAVE');
 
         // format chunk identifier
         // FMT sub-chunk
-        utils.writeUTFBytes(this.view, 12, 'fmt ')
+        utils.writeUTFBytes(this.view, 12, 'fmt ');
 
         // format chunk length
-        this.view.setUint32(16, 16, true)
+        this.view.setUint32(16, 16, true);
 
         // sample format (raw)
-        this.view.setUint16(20, 1, true)
+        this.view.setUint16(20, 1, true);
 
         // stereo (2 channels)
-        this.view.setUint16(22, 2, true)
+        this.view.setUint16(22, 2, true);
 
         // sample rate
-        this.view.setUint32(24, 44100, true)
+        this.view.setUint32(24, 44100, true);
 
         // byte rate (sample rate * block align)
-        this.view.setUint32(28, 44100 * 2, true)
+        this.view.setUint32(28, 44100 * 2, true);
 
         // block align (channel count * bytes per sample)
-        this.view.setUint16(32, 2 * 2, true)
+        this.view.setUint16(32, 2 * 2, true);
 
         // bits per sample
-        this.view.setUint16(34, 16, true)
+        this.view.setUint16(34, 16, true);
 
         // data sub-chunk
         // data chunk identifier
-        utils.writeUTFBytes(this.view, 36, 'data')
+        utils.writeUTFBytes(this.view, 36, 'data');
 
         // data chunk length
-        this.view.setUint32(40, length, true)
+        this.view.setUint32(40, length, true);
     }
     writeFile(data) {
         // 写入PCM数据
-        let length = data.length
-        let index = 44
-        let volume = 1
+        let length = data.length,
+            index = 44,
+            volume = 1;
 
         for (let i = 0; i < length; i++) {
-            this.view.setInt16(index, data[i] * (0x7FFF * volume), true)
-            index += 2
-        }
+            this.view.setInt16(index, data[i] * (0x7FFF * volume), true);
+            index += 2;
+        };
     }
-}
+};
 
 // 录音器构造函数
 class Recorder {
     constructor() {
+
         // 录音机状态
-        this.status = 'pause'
+        this.status = 'pause';
 
         // 缓存仓库
         this.store = {
@@ -138,84 +147,85 @@ class Recorder {
             channel_left: [],
             // 右声道
             channel_right: []
-        }
+        };
 
         // 杂项数据
-        this.pipe = {}
+        this.pipe = {};
     }
 
     clearStore() {
         // 清空数据
-        this.store.channel_left.splice(0)
-        this.store.channel_right.splice(0)
+        this.store.channel_left.splice(0);
+        this.store.channel_right.splice(0);
     }
 
     record(buffer) {
         // 记录缓存录音数据
-        let channel_left = buffer.getChannelData(0)
-        let channel_right = buffer.getChannelData(1)
+        let channel_left = buffer.getChannelData(0),
+            channel_right = buffer.getChannelData(1);
 
         // 双声道
-        this.store.channel_left.push(channel_left.slice(0))
-        this.store.channel_right.push(channel_right.slice(0))
+        this.store.channel_left.push(channel_left.slice(0));
+        this.store.channel_right.push(channel_right.slice(0));
     }
 
     start(record_config = {}) {
+
         // 录制中，不响应操作
-        if (this.status === 'recording') return new Error('In recording!')
+        if (this.status === 'recording') return new Error('In recording!');
 
         // 非录制中，开始录音操作
 
         // 清除上次缓存
-        this.clearStore()
+        this.clearStore();
 
         // 更改状态为录制中
-        this.status = 'recording'
+        this.status = 'recording';
 
         // 录音API getUserMedia 参数配置
         let option = Object.assign({
             sampleRate: 44100, // 采样率
             channelCount: 2, // 声道
             volume: 1.0 // 音量
-        }, record_config)
+        }, record_config);
 
         // 调用媒体API，向用户请求麦克风使用权
         let recorder = navigator.mediaDevices.getUserMedia({
             // 合并参数
             audio: option
-        })
+        });
 
         // 获得权限后开始操作
         recorder.then((mediaStream) => {
             this.handler(mediaStream)
-        })
+        });
 
         // 用户拒绝授权，重置状态并抛出异常信息
         recorder.catch(() => {
-            this.status = 'pause'
-            throw new Error('Users refuse authorization to use microphones.')
-        })
+            this.status = 'pause';
+            throw new Error('Users refuse authorization to use microphones.');
+        });
     }
 
     stop() {
         // 非录制中，不响应操作
-        if (this.status !== 'recording') return Promise.reject(new Error('Run start before stop'))
-        this.status = 'pause'
+        if (this.status !== 'recording') return Promise.reject(new Error('Run start before stop'));
+        this.status = 'pause';
 
         // 停止录音
-        this.pipe.mediaStream.getAudioTracks()[0].stop()
-        this.pipe.mediaSource.disconnect()
-        this.pipe.audioNode.disconnect()
+        this.pipe.mediaStream.getAudioTracks()[0].stop();
+        this.pipe.mediaSource.disconnect();
+        this.pipe.audioNode.disconnect();
 
         // 整合缓存区的数据
-        let left = utils.mergeArray(this.store.channel_left)
-        let right = utils.mergeArray(this.store.channel_right)
+        let left = utils.mergeArray(this.store.channel_left),
+            right = utils.mergeArray(this.store.channel_right);
 
         // 声道数据合并
-        let datas = utils.mergeChannel(left, right)
+        let datas = utils.mergeChannel(left, right);
 
         // 转换并返回wav录音数据(blob)
-        return new Wav(datas)
+        return new Wav(datas);
 
     }
 
@@ -238,30 +248,30 @@ class Recorder {
          */
 
         // 录音数据流
-        this.pipe.mediaStream = mediaStream
+        this.pipe.mediaStream = mediaStream;
 
         // 音频API，作用等同audio标签，详细=>http://t.cn/RvQZCuy
-        this.pipe.audioContext = new(window.AudioContext || window.webkitAudioContext)()
+        this.pipe.audioContext = new(window.AudioContext || window.webkitAudioContext)();
 
         // 转换媒体源，接收媒体流对象后，可播放或操作音频
-        this.pipe.mediaSource = this.pipe.audioContext.createMediaStreamSource(mediaStream)
+        this.pipe.mediaSource = this.pipe.audioContext.createMediaStreamSource(mediaStream);
 
         // 音频API，输入音频，可加工音源后输出
-        this.pipe.audioNode = utils.createAudioNode(this.pipe.audioContext)
+        this.pipe.audioNode = utils.createAudioNode(this.pipe.audioContext);
 
         /*
          * audioContext.destination是音频要最终输出的目标，可以把它理解为声卡
          * 所有节点中的最后一个节点应该再连接到audioContext.destination才能听到声音。
          */
-        this.pipe.audioNode.connect(this.pipe.audioContext.destination)
+        this.pipe.audioNode.connect(this.pipe.audioContext.destination);
 
         // 当音频推送过来时，缓存录音数据
         this.pipe.audioNode.addEventListener('audioprocess', event => {
             this.record(event.inputBuffer)
-        }, false)
+        }, false);
 
         // 最后把mediaNode连接到audioNode, 完成流程。
         // audioNode不会有数据返回，mediaSource不会在录音期间播放声音
-        this.pipe.mediaSource.connect(this.pipe.audioNode)
+        this.pipe.mediaSource.connect(this.pipe.audioNode);
     }
 }
